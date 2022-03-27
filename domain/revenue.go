f849ab3e092e15e691f943125ad9c35637918418
Lucas de Oliveira e Oliveira <lucasdeoliveiraeoliveira@DEV-MC028.local>
2022-03-27 14:02:26 -0300

    excluding date from revenue

--- domain/revenue.go
+++ domain/revenue.go
@@ -7,12 +7,13 @@ import (
 )
 
 type Revenue struct {
-	ID        uint           `json:"id" gorm:"primaryKey"`
-	Voucher   float32        `json:"voucher"`
-	Debit     float32        `json:"debit"`
-	Credit    float32        `json:"credit"`
-	Delivery  float32        `json:"delivery"`
-	Total     float32        `json:"total"`
+	ID       uint    `json:"id" gorm:"primaryKey"`
+	Voucher  float32 `json:"voucher"`
+	Debit    float32 `json:"debit"`
+	Credit   float32 `json:"credit"`
+	Delivery float32 `json:"delivery"`
+	Total    float32 `json:"total"`
+	//Date      string         `json:"date"`
 	CreatedAt time.Time      `json:"created"`
 	UpdatedAt time.Time      `json:"updated"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted"`
